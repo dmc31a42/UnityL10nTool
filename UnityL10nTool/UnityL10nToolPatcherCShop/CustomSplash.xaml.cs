d59b9539661bb2f33326cd35cbd7017a9a7be61f
NakWon CHOI <ch0494@gmail.com>
2018-10-27 16:26:09 +0900

    Patcher Splash Window add

---
+++ UnityL10nTool/UnityL10nToolPatcherCShop/CustomSplash.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UnityL10nToolPatcherCShop
+{
+    /// <summary>
+    /// CustomSplash.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class CustomSplash : Window
+    {
+        public JObject patcherJson;
+        public CustomSplash()
+        {
+            InitializeComponent();
+            string jsonStr = System.IO.File.ReadAllText("setting.json");
+            patcherJson = JObject.Parse(jsonStr);
+            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "splash.png"))
+            {
+                Uri uri = new Uri(AppDomain.CurrentDomain.BaseDirectory + "splash.png", UriKind.RelativeOrAbsolute);
+                SplashImage.Source = BitmapFrame.Create(uri);
+            }
+        }
+
+        private async void WailtFor2Second()
+        {
+            await Task.Delay(2000);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (patcherJson.ContainsKey("OnlineUpdate"))
+            {
+                JToken onlineUpdateJson = patcherJson["OnlineUpdate"];
+
+            }
+        }
+    }
+}
